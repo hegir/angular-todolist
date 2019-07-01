07094b8947adedaa8e198e7d484593d3c0703127
Abid Karač <abid.karac@edu.fit.ba>
2019-07-01 03:45:34 +0200

    Add files via upload

---
+++ components/todo-item/todo-item.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit , Input, EventEmitter, Output} from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../serverices/todo.service';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input() todo:Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+  }
+
+  setClasses(){
+    let classes = {
+      todo:true,
+      'is-complete':this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo){
+    todo.completed = !todo.completed;
+    this.todoService.toggleCompleted(todo).subscribe(todo =>
+      console.log('toggled'))
+  }
+
+  onDelete(todo){
+    this.deleteTodo.emit(todo);
+  }
+}
---
+++ serverices/todo.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+
+import { Todo } from '../models/Todo';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos'
+  todosLimit = '?_limit=5';
+
+  constructor(private http:HttpClient) { }
+
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  toggleCompleted(todo:Todo):Observable<any>{
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url,todo,httpOptions);
+  }
+
+  deleteTodo(todo:Todo):Observable<Todo>{
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url,httpOptions)
+  }
+
+  addTodo(todo):Observable<Todo>{
+    return this.http.post<Todo>(this.todosUrl, todo,httpOptions)
+  }
+}
